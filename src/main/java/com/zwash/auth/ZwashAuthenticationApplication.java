5f95460287abd1e856e600c315522c9c3d9ee173
Zeriabo <zeriab@hotmail.com>
2023-09-14 14:25:19 +0300

    authentication service

---
+++ src/main/java/com/zwash/auth/ZwashAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.zwash.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwashAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZwashAuthenticationApplication.class, args);
+	}
+
+}
