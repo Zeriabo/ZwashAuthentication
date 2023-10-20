cde7d9563eaffd32d8eb21744cf01620e8f7446d
Zeriabo <zeriab@hotmail.com>
2023-10-20 15:59:27 +0300

    Update ZwashAuthenticationApplication.java

--- src/main/java/com/zwash/auth/ZwashAuthenticationApplication.java
+++ src/main/java/com/zwash/auth/ZwashAuthenticationApplication.java
@@ -3,10 +3,12 @@ package com.zwash.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @ComponentScan(basePackages = {"com.zwash.common","com.zwash.auth"})
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ZwashAuthenticationApplication {
 
 	public static void main(String[] args) {
