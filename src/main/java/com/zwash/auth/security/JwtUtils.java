3e3e615c4deefbbfa172fd962d6468cde45f5d5d
Zeriab Chiah <zeriab@Zeriabs-MBP.lan>
2025-06-18 15:46:05 +0300

    move some functionalities to ZwashCommon

--- src/main/java/com/zwash/auth/security/JwtUtils.java
+++ src/main/java/com/zwash/auth/security/JwtUtils.java
@@ -1,15 +1,10 @@
 package com.zwash.auth.security;
 
-import java.security.Key;
 import java.util.Date;
-
 import javax.crypto.SecretKey;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
